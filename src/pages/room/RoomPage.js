e44b8d98f5b535b39e6b730d82c79428eba7f4e3
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-02-08 23:11:13 +0200

    Added banner component to homepage, roompage and errorpage

--- src/pages/room/RoomPage.js
+++ src/pages/room/RoomPage.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import Hero from '../components/hero/Hero';
-import Banner from '../components/banner/Banner';
+import styles from './roomPage.module.css';
+import Hero from '../../components/hero/Hero';
+import Banner from '../../components/banner/Banner';
 
-const ErrorPage = () => {
+const RoomPage = () => {
     return (
         <div>
-            <Hero>
-                <Banner title="404" subtitle="page not found">
+            <Hero hero={styles.roomsHero}>
+                <Banner title="our rooms">
                     <Link to="/" className="btn-primary">
                         return home
                     </Link>
                 </Banner>
             </Hero>
-
         </div>
     )
 }
 
-export default ErrorPage
+export default RoomPage
