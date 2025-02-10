ba458912abfe898de44a14d28d095b9796133002
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-02-10 23:16:18 +0200

    Modify the room context to handle room filtering

--- src/context.js
+++ src/context.js
@@ -4,7 +4,22 @@ import React, { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import items from './data';
 
-export const RoomContext = createContext()
+export const RoomContext = createContext();
+
+const initialFormState = {
+  type: 'all',
+  capacity: 1,
+  price: 0,
+  minPrice: 0,
+  maxPrice: 0,
+  minSize: 0,
+  maxSize: 0,
+  breakfast: false,
+  pets: false,
+};
+
+const [values, setValues] = useState(initialFormState);
+
 
 const RoomContextProvider = props => {
   const [rooms, setRooms] = useState([]);
@@ -25,30 +40,83 @@ const RoomContextProvider = props => {
 
   useEffect(() => {
     const roomsArray = formatData(items);
+    const maxPrice = Math.max(...rooms.map(item => item.price));
+    const maxSize = Math.max(...rooms.map(item => item.size));
     const roomData = () => {
       setRooms(roomsArray);
       setFeaturedRooms(roomsArray.filter(room => room.featured === true));
       setSortedRooms(roomsArray);
       setLoading(false);
+      setValues(initialFormState.price = maxPrice);
+      setValues(initialFormState.maxPrice = maxPrice);
+      setValues(initialFormState.maxSize = maxSize);
     };
     roomData();
-  }, []);
+  }, [])
+  return room;
+};
 
-  const getRooms = slug => {
-    const room = [...rooms].find(room => room.slug === slug);
-    return room;
-  };
+const filterRooms = () => {
+  let {
+    capacity,
+    price,
+  } = values;
+  const {
+    type, minSize, maxSize, breakfast, pets,
+  } = values;
+  let tempRooms = [...rooms];
+
+  capacity = parseInt(capacity, 10);
+  price = parseInt(price, 10);
+
+  if (values.type !== 'all') {
+    tempRooms = tempRooms.filter(room => room.type === type);
+  }
+
+  if (capacity !== 1) {
+    tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+  }
+
+  tempRooms = tempRooms.filter(room => room.price <= price);
+
+  tempRooms = tempRooms.filter(
+    room => room.size >= minSize && room.size <= maxSize,
+  );
+
+  if (breakfast) {
+    tempRooms = tempRooms.filter(room => room.breakfast === true);
+  }
+
+  if (pets) {
+    tempRooms = tempRooms.filter(room => room.pets === true);
+  }
+  setSortedRooms(tempRooms);
+};
+
+const handleChange = event => {
+  const { target } = event;
+  const value = target.type === 'checkbox' ? target.checked : target.value;
+  const { name } = target;
+
+  setValues(
+    {
+      [name]: value,
+    },
+    filterRooms,
+  );
+};
+
 
   return (
 
     <RoomContext.Provider value={{
       rooms, featuredRooms, sortedRooms, loading, getRooms,
+      rooms, featuredRooms, sortedRooms, loading, getRooms, handleChange,
     }}
     >
       {props.children}
     </RoomContext.Provider>
   );
-};
 
 RoomContextProvider.propTypes = {
   children: PropTypes.instanceOf(Array).isRequired,
